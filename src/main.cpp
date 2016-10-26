0dc984e43757026ca0d17682fe0aff09478db61f
xruyn <pavelrzn88@gmail.com>
2016-10-26 17:19:56 +0300

    pins

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,125 @@
 
    Стоим лицом ко входу в зал, слева направо - (коридор) 1, 2, 3 (кухня)
 
+   режим 1 - умный режим, включение по датчику движения и только при выключенном основном
+   режим 2 - просто включить все, без датчиков
+   режим 3 - просто выключить все
  */
 #include <Arduino.h>
 #include <RTClib.h>
 #include <Wire.h>
 RTC_DS1307 rtc;       //выбор DS
+void lighting_enable();
+void lighting_disable();
+void lighting_middle();
+void pit_test();
+void pulsout(byte x, int y);
+void digitOut(byte j);
+void digit_mode(int digit);
 
 
-byte pin_relay_1 = 9; //пин для управление реле1 (выход)
-byte pin_relay_2 = 8; //пин для управление реле2 (выход)
-byte pin_relay_3 = 7; //пин для управление реле3 (выход)
-byte pin_relay_4 = 6; //пин для управление реле4 (выход)
-byte pin_220v = 5; //пин для подключения оптопары (вход ADC)
-byte pin_PIR = 11;    //пин для подключаения PIR (вход)
-byte pin_button = 10; //пин для подключения кнопки без фиксации
-byte pin_mode_1 = A0; //режим 1 - умный режим, включение по датчику движения и только при выключенном основном
-byte pin_mode_2 = 13; //режим 2 - просто включить все, без датчиков
-byte pin_mode_3 = 12; //режим 3 - просто выключить все
+#define pin_relay_1 9 //пин для управление реле1 (выход)
+#define pin_relay_2 8 //пин для управление реле2 (выход)
+#define pin_relay_3 7 //пин для управление реле3 (выход)
+#define pin_relay_4 6 //пин для управление реле4 (выход)
+#define pin_220v  5 //пин для подключения оптопары (вход)
+#define pin_PIR 11    //пин для подключаения PIR (вход)
+#define pin_button 10 //пин для подключения кнопки без фиксации
+#define pin_data 12 //data HC164
+#define pin_CLK A1 //CLK HC164
+#define pin_nCLR A0 //nCLR HC164
 
 int pirState = LOW;
 int val_PIR = 0;
 int flag = 0;
-int val=0;
+int val = 0;
 int button_state = 0;
 float delta;
 float time1;
+byte Digit[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67};
+
+
+
+
+void setup() {
+        //Serial.begin(9600);
+        Wire.begin();
+        rtc.begin();
+        pinMode(pin_relay_1, OUTPUT);
+        pinMode(pin_relay_2, OUTPUT);
+        pinMode(pin_relay_3, OUTPUT);
+        pinMode(pin_relay_4, OUTPUT);
+        pinMode(pin_220v, INPUT);
+        pinMode(pin_PIR, INPUT);
+        pinMode(pin_button, INPUT);
+        pinMode(pin_data, OUTPUT);
+        pinMode(pin_CLK, OUTPUT);
+        pinMode(pin_nCLR, OUTPUT);
+        pinMode(13, OUTPUT);
+
+        digitalWrite(pin_nCLR, HIGH);
+        for (int p = 9; p >= 0; p--) {
+                digitOut(Digit[p]);
+                delay(100);
+        }
+
+
+
+}
+
+void loop() {
+        DateTime now = rtc.now();
+
+
+
+
+        if (button_state == 0) {     //основной, "умный режим, включение по датчику движения и только при выключенном основном"
+                digit_mode(1);
+                if (digitalRead(pin_PIR) == HIGH && digitalRead(pin_220v) == LOW) {
+                        Serial.println("Motion, svet");
+                        if (now.hour() > 5 && now.hour() < 19) {
+                                lighting_enable();
+                                Serial.println(now.hour());
+                                Serial.println("Svet vse");
+                        }
+                        else { lighting_middle(); Serial.println("Gorit sredni"); }
+                }
+                else { lighting_disable(); Serial.println("Bez podsvetki"); }
+        }
+
+        if(digitalRead(pin_button) == HIGH && flag == 0) {
+                button_state++;
+                flag = 1;
+                if(button_state>2)    {
+                        button_state = 0;
+                }
+        }
+
+        if(digitalRead(pin_button) == LOW && flag == 1) {
+                flag = 0;
+        }
+
+        if (button_state == 1) {  // режим "просто включить все, без датчиков"
+                digit_mode(2);
+                lighting_enable();
+                Serial.println("Vse +");
+        }
+
+        if (button_state == 2) {  // режим "просто выключить все"
+                digit_mode(3);
+                lighting_disable();
+                Serial.println("Vse -");
+        }
+
+}
+
 
 void lighting_enable() {
         digitalWrite(pin_relay_1, LOW);
         digitalWrite(pin_relay_2, LOW);
         digitalWrite(pin_relay_3, LOW);
         digitalWrite(pin_relay_4, LOW);
+
 }
 void lighting_disable() {
         digitalWrite(pin_relay_4, HIGH);
@@ -76,62 +164,34 @@ void pir_test(){
                 }
         }
 }
-void setup() {
-        Serial.begin(9600);
-        Wire.begin();
-        rtc.begin();
-        pinMode(pin_relay_1, OUTPUT);
-        pinMode(pin_relay_2, OUTPUT);
-        pinMode(pin_relay_3, OUTPUT);
-        pinMode(pin_relay_4, OUTPUT);
-        pinMode(pin_220v, INPUT);
-        pinMode(pin_PIR, INPUT);
-        pinMode(pin_button, INPUT);
-        pinMode(pin_mode_1, OUTPUT);
-        pinMode(pin_mode_2, OUTPUT);
-        pinMode(pin_mode_3, OUTPUT);
-        pinMode(13, OUTPUT);
+
+void digitOut(byte j)   {       // вывод цифра на индикатор
+        byte k;
+        for(int i=0; i < 8; i++)   {
+                k = j & 0x01;
+                digitalWrite(pin_data, !k); // для общего катода убрать !
+                pulsout(pin_CLK, 1);
+                j = j >> 1;
+        }
 
 }
 
-void loop() {
-        DateTime now = rtc.now();
-        if (button_state == 0) {     //основной, "умный режим, включение по датчику движения и только при выключенном основном"
-                digitalWrite(pin_mode_1, LOW);
-                if (digitalRead(pin_PIR) == HIGH && digitalRead(pin_220v) == LOW) {
-                        Serial.println("Motion, svet");
-                        if (now.hour() > 5 && now.hour() < 19) {
-                                lighting_enable();
-                                Serial.println(now.hour());
-                                Serial.println("Svet vse");
-                        }
-                        else { lighting_middle(); Serial.println("Gorit sredni"); }
-                }
-                else { lighting_disable(); Serial.println("Bez podsvetki"); }
-        }
 
-        if(digitalRead(pin_button) == HIGH && flag == 0) {
-                button_state++;
-                flag = 1;
-                if(button_state>2)    {
-                        button_state = 0;
-                }
-        }
 
-        if(digitalRead(pin_button) == LOW && flag == 1) {
-                flag = 0;
-        }
+void pulsout(byte x, int y)   { // функция CLK для HC164
+        byte z = digitalRead(x);
+        z = !z;
+        digitalWrite(x, z);
+        delayMicroseconds(10* y);
+        z = !z; // return to original state
+        digitalWrite(x, z);
+        return;
+}
 
-        if (button_state == 1) {  // режим "просто включить все, без датчиков"
-                digitalWrite(pin_mode_2, LOW);
-                lighting_enable();
-                Serial.println("Vse +");
-        }
 
-        if (button_state == 2) {  // режим "просто выключить все"
-                digitalWrite(pin_mode_3, LOW);
-                lighting_disable();
-                Serial.println("Vse -");
+void digit_mode(int digit){
+        for (int p = digit; p <= digit; p++) {
+                digitOut(Digit[p]);
+                delay(10);
         }
-
 }
