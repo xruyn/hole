36d835e01089d4527354f518540e8ddfb0e840c9
xruyn <pavelrzn88@gmail.com>
2016-11-01 17:19:04 +0300

    2

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,9 @@
 #include <RTClib.h>
 #include <Wire.h>
 #include <MsTimer2.h>
+
 RTC_DS1307 rtc;       //выбор DS
+
 void lighting_enable();
 void lighting_disable();
 void lighting_middle();
@@ -37,7 +39,7 @@ void low_battery();
 #define pin_nCLR A0 //nCLR HC164
 #define pin_ADC A7 // вход АЦП для измерения напряжения батарейки DS1307
 
-int pirState = LOW;
+boolean pirState = LOW;
 int val_PIR = 0;
 int flag = 0;
 int val = 0;
@@ -45,7 +47,7 @@ int button_state = 0;
 float scale = 0.00447; // реальное напряжение Vcc/1023
 float delta;
 float time1;
-byte Digit[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67};
+byte Digit[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67, 0x79};
 
 
 
@@ -54,7 +56,7 @@ void setup() {
         Serial.begin(9600);
         Wire.begin();
         rtc.begin();
-        MsTimer2::set(7000, low_battery); // 7s период
+        MsTimer2::set(5000, low_battery); // 7s период
         MsTimer2::start();
         pinMode(pin_relay_1, OUTPUT);
         pinMode(pin_relay_2, OUTPUT);
@@ -67,7 +69,7 @@ void setup() {
         pinMode(pin_CLK, OUTPUT);
         pinMode(pin_nCLR, OUTPUT);
         pinMode(13, OUTPUT);
-        analogReference(DEFAULT);
+        analogReference(INTERNAL);
         pinMode(pin_ADC, INPUT);
         digitalWrite(pin_nCLR, HIGH);
         for (int p = 9; p >= 0; p--) {
@@ -78,30 +80,28 @@ void setup() {
 
 
 void loop() {
-        //digitalWrite(pin_nCLR, HIGH);
+        digitalWrite(pin_nCLR, HIGH);
+        digit_mode(1);
+
         DateTime now = rtc.now();
-
-
-
-
         if (button_state == 0) {     //основной, "умный режим, включение по датчику движения и только при выключенном основном"
                 //digit_mode(1);
                 if (digitalRead(pin_PIR) == HIGH && digitalRead(pin_220v) == LOW) {
-                        Serial.println("Motion, svet");
+                        //Serial.println("Motion, svet");
                         if (now.hour() > 5 && now.hour() < 19) {
                                 lighting_enable();
                                 Serial.println(now.hour());
-                                Serial.println("Svet vse");
+                                //Serial.println("Svet vse");
                         }
                         else {
                                 lighting_enable();
                                 lighting_middle();
-                                Serial.println("Gorit sredni");
+                                //Serial.println("Gorit sredni");
                         }
                 }
                 else {
                         lighting_disable();
-                        Serial.println("Bez podsvetki");
+                        //Serial.println("Bez podsvetki");
                 }
         }
 
@@ -120,13 +120,13 @@ void loop() {
         if (button_state == 1) {  // режим "просто включить все, без датчиков"
                 //digit_mode(2);
                 lighting_enable();
-                Serial.println("Vse +");
+                //Serial.println("Vse +");
         }
 
         if (button_state == 2) {  // режим "просто выключить все"
                 //digit_mode(3);
                 lighting_disable();
-                Serial.println("Vse -");
+                //Serial.println("Vse -");
         }
 
 }
@@ -158,7 +158,7 @@ void pir_test(){
                 digitalWrite(13, HIGH); // включаем светодиод
                 if (pirState == LOW) {
                         // мы только что включили
-                        Serial.println("Motion detected!");
+                        //Serial.println("Motion detected!");
                         time1 = millis();
                         // мы выводим на серийный монитор изменение, а не состояние
                         pirState = HIGH;
@@ -168,7 +168,7 @@ void pir_test(){
                 digitalWrite(13, LOW); // выключаем светодиод
                 if (pirState == HIGH) {
                         // мы только что его выключили
-                        Serial.println("Motion ended!");
+                        //Serial.println("Motion ended!");
                         delta = millis() - time1;
                         Serial.println(delta);
                         // мы выводим на серийный монитор изменение, а не состояние
@@ -210,12 +210,17 @@ void digit_mode(int digit){
 }
 
 void low_battery(){
-  static boolean output = HIGH;
+        volatile float average = 0;
+        for(int i = 0; i < 5; i++) {
+                average += analogRead(pin_ADC);
+        }
+        Serial.println((1.057*(average/5)/1023)/0.09157);
+        //Serial.println(scale*(average/5));
+        if (scale*(average/5) < 3.20) {
+                static boolean output = HIGH;
+                digitalWrite(13, output);
+                output = !output;
+        }
 
-  digitalWrite(13, output);
-  output = !output;
-      // float v_bat = analogRead(pin_ADC); // читаем значение на аналоговом входе
-      // if (v_bat*scale < 2.7) {
-      //
-      // }
+        //return;
 }
